b36c294480a7d3dcc656af8e82ac92e560585d89
manassogale132 <73747525+manassogale132@users.noreply.github.com>
2020-11-17 12:41:00 +0530

    Main

--- EmpWageBuilderInterface/src/EmpWageInterface/Main.java
+++ EmpWageBuilderInterface/src/EmpWageInterface/Main.java
@@ -6,11 +6,22 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner scan=new Scanner(System.in);
+		
 		System.out.println("-Employee Wage Builder-");
 		
+		//Taking input for user.
+		//Making separate object for each company i.e(company 1,company 2,company 3) using interface.
 		
-		
-		empwageinterface company1=new companies();
-        company1.comp1();
+        empwageinterface company1=new companies();
+        company1.comp1();                                         //1st Company
+        
+        empwageinterface company2=new companies();
+        company2.comp1();                                         //2nd Company
+        
+        empwageinterface company3=new companies();
+        company3.comp1();                                         //3rd Company
+       
+        
 }
 }
+
